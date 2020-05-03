4d460025ce8a28f63541ca80d50a6e8ab6947110
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2020-05-03 09:17:51 +0300

    add merge without comparator

--- lab_16/include/parallel_sort.hpp
+++ lab_16/include/parallel_sort.hpp
@@ -6,27 +6,53 @@
 
 namespace lab_16 {
 
+namespace inner {
+
 template<class RandomIt>
-void merge_sort(RandomIt first, RandomIt last) {
-    if (last - first > 2) {
-	RandomIt middle = RandomIt((last + first) / 2);
-	std::merge(first, middle, middle, last, first);
-    }
-    else if (last - first == 2 && *last < *first) {
-	std::swap(*last, *first);
+void expansion_merge(RandomIt first1, RandomIt last1, RandomIt last2) {
+    std::vector<typename std::iterator_traits<RandomIt>::value_type> merge_vec(last2 - first1);
+    std::merge(first1, last1, last1, last2, merge_vec.begin());
+    for (RandomIt i = merge_vec.begin(), j = first1; i < merge_vec.end(); ++i, ++j) {
+	std::swap(*j, *i);
     }
 }
 
 template<class RandomIt>
-void parallel_sort(std::size_t nthreads, RandomIt first, RandomIt last) {
-    std::vector<std::thread> threads;
-    std::size_t step = (last - first) / nthreads;
-    for (std::size_t i = 0; i < nthreads; ++i) {
-	threads.emplace_back(merge_sort, first + i * step, first + (i + 1) * step);
+void merge_sort(RandomIt first, RandomIt last) {
+    if (last - first > 2) {
+	std::size_t d_mid = (last - first)/2;
+
+	merge_sort(first, first + d_mid);
+	merge_sort(first + d_mid, last);
+	expansion_merge(first, first + d_mid, last);
     }
-    for (std::size_t i = 0; i < nthreads; i++) {
-	threads[i].join();
-	std::merge(first, first + i * step, first + i * step, first + i * step, first + (i + 1) * step);
+    else if (last - first == 2 && *first > *(last - 1)) {
+	std::swap(*(last - 1), *first);
+    }
+}
+
+}
+
+template<class RandomIt>
+void parallel_sort(std::size_t nthreads, RandomIt first, RandomIt last) {
+    std::vector<std::thread> vt;
+    std::size_t step = (last - first) / nthreads;
+    std::vector<RandomIt> borders(nthreads + 1);
+
+    for (std::size_t i = 0; i <= nthreads; ++i) {
+	if (i == nthreads)
+	    borders[i] = last;
+	else
+	    borders[i] = first + i * step;
+    }
+
+    for (std::size_t i = 0; i < nthreads; ++i) {
+	vt.emplace_back(inner::merge_sort<RandomIt>, borders[i], borders[i+1]);
+    }
+
+    for (std::size_t i = 0; i < nthreads; ++i) {
+	vt[i].join();
+	inner::expansion_merge(first, borders[i], borders[i+1]);
     }
 }
 
--- lab_16/src/main.cpp
+++ lab_16/src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
+#include "parallel_sort.hpp"
 
 int main()
 {
     std::vector<int> v = {5, 2, 8, 9, 4, 1};
     std::vector<int> sorted_v = {1, 2, 4, 5, 8, 9};
+    std::vector<int> :: iterator beg = v.begin();
+    std::vector<int> :: iterator end = v.end();
+    lab_16::parallel_sort<std::vector<int>::iterator>(3, beg, end);
+    for (auto i : v) {
+	std::cout << i << " " << std::endl;
+    }
     return 0;
 }
